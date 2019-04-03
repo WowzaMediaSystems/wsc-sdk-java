1cac840e0bba956d91ce4e3c66527ff238aa5e1b
Andrew S. Kennedy <andrew@wowza.com>
2019-04-03 23:37:19 +0100

    stop circleci from self running on each zip file update

--- .circleci/helpers/commit-built-files.sh
+++ .circleci/helpers/commit-built-files.sh
@@ -4,6 +4,6 @@ echo "commit-built-files.sh"
 
 git add target/WowzaCloudSDK-java-client-1.3.0.zip
 
-git commit -m "Wowza Streaming Cloud Java SDK Version 1.3.0 build ${CURRENT}"
+git commit -m "Wowza Streaming Cloud Java SDK Version 1.3.0 build ${CURRENT} [ci skip]"
 
 git push --set-upstream origin development
