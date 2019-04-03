f8b63e5702d0de784d7cfc750da96795c9a8f5c4
Andrew S. Kennedy <andrew@wowza.com>
2019-04-03 22:58:44 +0100

    Need to look at this more - seems off for the push

--- .circleci/helpers/commit-built-files.sh
+++ .circleci/helpers/commit-built-files.sh
@@ -6,4 +6,4 @@ git add target/WowzaCloudSDK-java-client-1.3.0.zip
 
 git commit -m "Wowza Streaming Cloud Java SDK Version 1.3.0 build ${CURRENT}"
 
-git push origin
+git push --set-upstream origin development
