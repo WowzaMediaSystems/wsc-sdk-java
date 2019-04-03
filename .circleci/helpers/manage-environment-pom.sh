c2088e1328404490fcadfba619da494426c5a079
Andrew S. Kennedy <andrew@wowza.com>
2019-04-03 22:08:56 +0100

    Fixing a typo - for the environment variable

--- .circleci/helpers/manage-environment-pom.sh
+++ .circleci/helpers/manage-environment-pom.sh
@@ -2,7 +2,9 @@
 
 echo "manage-environment-pom.sh"
 
-pom_locaton=~/pom.xml
+pom_location=~/pom.xml
+
+echo "Pom location ${pom_location}"
 
 CURRENT=$(date +%Y%m%d%H%M%S)
 sed -i 's/BUILDEXTENDVERSION/$CURRENT/' ${pom_location}
