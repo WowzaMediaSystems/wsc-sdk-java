e5c4353902e8904ee07623d6df708659ca98d3d1
Andrew S. Kennedy <andrew@wowza.com>
2019-04-04 15:07:04 +0100

    Break test to make sure test monitoring works

--- src/test/java/com/wowza/cloudsdk/test/client/model/AkamaiStreamSourceCreateInputModelTest.java
+++ src/test/java/com/wowza/cloudsdk/test/client/model/AkamaiStreamSourceCreateInputModelTest.java
@@ -34,6 +34,6 @@ public class AkamaiStreamSourceCreateInputModelTest {
 	StreamSourceAkamai streamSourceAkamai = new StreamSourceAkamai();
      	streamSourceAkamai.setId("mySetId");
 	modelObject.setStreamSourceAkamai(streamSourceAkamai);
-	assertEquals(modelObject.getStreamSourceAkamai().getId(),"mySetId");
+	assertEquals(modelObject.getStreamSourceAkamai().getId(),"mySetID");
      }
 }
